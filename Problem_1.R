14a5ff027d295f4158c6b11366af4bb5b058a839
emakarieva <ely.makarieva@gmail.com>
2018-03-30 14:47:15 +0300

    700862

--- Problem_1.R
+++ Problem_1.R
@@ -1,1 +1,28 @@
+# a)
+x <-  c( 4,1,1,4)
+# b)
+y <-  c(1,4)
+
+#c)
+# Define difference 
+difference <-  x-y
+
+# d)
+s <- c(x,y)
+
+# e)
+rep(s,10)
+length(rep(s,10))
+
+# f)
+rep(s, each = 3)
+
+# g) 
+seq(7,21)
+7:21
+
+#h)
+length(seq(7,21))
+
+
 
